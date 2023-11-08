160dac6b34de187e0e4333280cb5556cb29f65dc
Wairimu <charitykabuu21@gmail.com>
2023-11-08 12:20:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('product-registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle product registration form submission
+    // You can use JavaScript or make API calls to register the product on the blockchain
+  });
+  
+  document.getElementById('tracking-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle tracking form submission
+    // You can use JavaScript or make API calls to track and trace the product's movement
+  });
+  
+  document.getElementById('quality-assurance-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle quality assurance form submission
+    // You can use JavaScript or make API calls to certify product quality and authenticity
+  });
+  
